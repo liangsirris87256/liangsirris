072716aac201995e7c74ac1a0336109c8e3e4060
liangsirris87256 <62292211+liangsirris87256@users.noreply.github.com>
2020-04-06 21:38:19 +0800

    Add files via upload

---
+++ 20200406/js/index.js
@@ -0,0 +1,77 @@
+(function(){
+   let navList=document.querySelectorAll('.navbar-link .nav-item'),
+   proudctBox=document.querySelector('.proudctBox'),
+   data=null,
+   cardList=null;
+
+
+   let xhr=new XMLHttpRequest;
+   xhr.open('GET', './json/product.json', false);
+   xhr.onreadystatechange=()=>{
+       if(xhr.readyState===4&&xhr.status===200){
+           data=JSON.parse(xhr.responseText);
+       }
+   }
+   xhr.send(null);
+    
+
+   let str=``;
+   for( let i=0;i<data.length;i++){
+      let item=data[i];
+   let {
+       id,
+       img,
+       hot,
+       time,
+       price
+
+   }=item;
+   str+= `<div class="card" data-price='${price}'data-hot='${hot}' data-time='${time}'>
+   <img src="${img}" >
+<h3>${id}</h3>
+<p class="text">${price}</p>
+<p class="text">${time}</p>
+<p class="text">${hot}已购买</p>
+</div>`
+   }
+    proudctBox.innerHTML=str;
+    cardList=proudctBox.querySelectorAll('.card');
+
+
+  for(let i=0;i<data.length;i++){
+      let item=navList[i];
+      item.flag=-1;
+      item.onclick=function(){
+         let arr=Array.from(navList);
+         for(let j=0;j>navList.length;j++){
+             if(navList[j]!==this){
+                navList[j].flag=-1;
+             }
+            }
+             this.flag*=-1;
+             let char='data-price';
+             i==1?char='data-hot':null
+             i==2?char='data-time':null
+             arr.sort((a,b)=>{
+                a = a.getAttribute(char);
+                b = b.getAttribute(char);
+                if(char=='data-time'){
+                    a=a.replace(/-/g,'')
+                    b=b.replace(/-/g,'')
+                }
+                return (a-b)*this.flag;
+             })
+             for(let z=0;z<arr.length;z++){
+                 proudctBox.appendChild(arr[z]);
+             
+
+         }
+      }
+  }
+   
+
+
+
+
+
+})();
